3eaf666a22da6224acbd08a36c88bdd0ffa2e66d
YashasviKotra18 <143369356+YashasviKotra18@users.noreply.github.com>
2023-09-19 00:19:40 -0700

    Update Graph_app_fish.py

--- Graph_app_fish.py
+++ Graph_app_fish.py
@@ -8,10 +8,11 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('Fish.csv')
+df = pd.read_csv('C:/Users/Checkout/Fish.csv')
 
-Fig = px.bar(df, x= 'Species', y = 'Weight')
-st.plotly_chart(fig)
+Fig = px.bar(df, x= 'Species', y = 'Weight', title = 'Bar plot depicting weights of different fish species')
+
+st.plotly_chart(Fig)
 
 
 # In[ ]:
